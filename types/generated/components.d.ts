efb58c4068ded154bcb0b9d5ec0ee69ad886c206
ezequiel.everton <ezequiel.everton@ceuma.br>
2024-05-11 14:59:39 -0300

    innitial commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,34 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface SectionSectionMetadata extends Schema.Component {
+  collectionName: 'components_section_section_metadata';
+  info: {
+    displayName: 'section_metadata';
+    icon: 'layer';
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 255;
+      }>;
+    section_id: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 50;
+      }>;
+    background: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<false>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'section.section-metadata': SectionSectionMetadata;
+    }
+  }
+}
